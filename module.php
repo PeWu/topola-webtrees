41a8e67996e505672c43b45a9c1b5311c3cae823
lilaelephant <84687112+lilaelephant@users.noreply.github.com>
2021-05-24 00:22:49 +0200

    Support for webtrees 2.0.x
    This adds support for webtrees 2.0.x. The last time I have touched PHP was more than 8 years ago.
    I've used the https://github.com/magicsunday/webtrees-fan-chart repository as heavy inspiration.
    
    Warning: the permission management isn't working properly yet. I don't know how to proceed there.

--- module.php
+++ module.php
@@ -1,107 +1,23 @@
 <?php
-namespace Fisharebest\Webtrees;
-
-use Fisharebest\Webtrees\Auth;
-use Fisharebest\Webtrees\I18N;
-use Fisharebest\Webtrees\Menu;
-use Fisharebest\Webtrees\Controller\ChartController;
-use Fisharebest\Webtrees\Functions\FunctionsExport;
-use Fisharebest\Webtrees\Module\AbstractModule;
-use Fisharebest\Webtrees\Module\ModuleChartInterface;
 
 /**
- * Opens the Topola Genealogy Viewer in embedded mode in an iframe.
- *
- * The Topola Genealogy Viewer is available at
- * https://pewu.github.io/topola-viewer
+ * See LICENSE.md file for further details.
  */
-class TopolaModule extends AbstractModule implements ModuleChartInterface {
-    public function getTitle(): string {
-        return
-            /* I18N: Name of a module */
-            I18N::translate('Interactive tree (Topola)');
-    }
 
-    public function getDescription(): string {
-        return
-            /* I18N: Description of the module */
-            I18N::translate(
-                'Opens the Topola Genealogy Viewer interactive familty tree.');
-    }
+declare(strict_types=1);
 
-    public function getChartMenu(Individual $individual) {
-		return new Menu(
-            $this->getTitle(),
-            'module.php?mod=' . $this->getName() .
-                '&amp;mod_action=view&amp;rootid=' . $individual->getXref() .
-                '&amp;ged=' . $individual->getTree()->getNameUrl(),
-			'menu-chart-ancestry',
-			array('rel' => 'nofollow')
-		);
-    }
+namespace LilaElephant\Webtrees;
 
-    public function getBoxChartMenu(Individual $individual) {
-        return null;
-    }
+use Composer\Autoload\ClassLoader;
+use LilaElephant\Webtrees\Topola\Module;
 
-    /** Exports the database as a GEDCOM file. */
-    private function gedcomAction() {
-        global $WT_TREE;
-        switch (Auth::accessLevel($WT_TREE)) {
-            case Auth::PRIV_NONE:
-                $privatize = 'gedadmin';
-                break;
-            case Auth::PRIV_USER:
-                $privatize = 'user';
-                break;
-            default:
-                $privatize = 'visitor';
-                break;
-        }
-        $options = array(
-            'privatize' => $privatize,
-            'toANSI' => false,
-            'path' => '',
-        );
-        $stream = fopen('php://output', 'w');
-        FunctionsExport::exportGedcom($WT_TREE, $stream, $options);
-    }
+// Register our namespace
+$loader = new ClassLoader();
+$loader->addPsr4(
+    'LilaElephant\\Webtrees\\Topola\\',
+    __DIR__ . '/src'
+);
+$loader->register();
 
-    /** Renders an iframe with Topola Genealogy Viewer in embedded mode. */
-    private function viewAction() {
-        global $WT_TREE;
-        global $controller;
-        $controller = new ChartController;
-        $controller
-            ->setPageTitle(
-                /* I18N: Page title for interactive tree */
-                I18N::translate('Interactive tree (Topola)'))
-            ->pageHeader()
-            ->addInlineJavascript(
-                'var GEDCOM_URL = "module.php?mod=' . $this->getName() .
-                '&mod_action=gedcom&ged=' . $WT_TREE->getNameUrl() . '";')
-            ->addExternalJavascript(
-                WT_STATIC_URL . WT_MODULES_DIR . $this->getName() .
-                    '/topola.js');
-        ?>
-        <iframe
-            id="topolaFrame"
-            style="width: 100%; height: 800px;"
-            src="https://pewu.github.io/topola-viewer/#/view?utm_source=webtrees&embedded=true">
-        </iframe>
-        <?php
-    }
-
-    public function modAction($mod_action) {
-        switch ($mod_action) {
-            case 'gedcom':
-                $this->gedcomAction();
-                break;
-            case 'view':
-                $this->viewAction();
-                break;
-        }
-    }
-}
-
-return new TopolaModule(__DIR__);
+// Create and return instance of the module
+return app(Module::class);
---
+++ src/Traits/ModuleCustomTrait.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * See LICENSE.md file for further details.
+ */
+
+declare(strict_types=1);
+
+namespace LilaElephant\Webtrees\Topola\Traits;
+
+use Fig\Http\Message\StatusCodeInterface;
+use Fisharebest\Localization\Translation;
+use Fisharebest\Webtrees\Registry;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+/**
+ * Trait ModuleCustomTrait.
+ *
+ * @author  Someone
+ * @inspiration  Rico Sonntag <mail@ricosonntag.de>
+ * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License v3.0
+ * @link    https://github.com/PeWu/topola-webtrees
+ */
+trait ModuleCustomTrait
+{
+    use \Fisharebest\Webtrees\Module\ModuleCustomTrait;
+
+    public function customModuleAuthorName(): string
+    {
+        return self::CUSTOM_AUTHOR;
+    }
+
+    public function customModuleVersion(): string
+    {
+        return self::CUSTOM_VERSION;
+    }
+
+    public function customModuleLatestVersionUrl(): string
+    {
+        return self::CUSTOM_LATEST_VERSION;
+    }
+
+    protected function extractVersion(string $content): string
+    {
+        return json_decode($content, true)['tag_name'] ?? '';
+    }
+
+    /**
+     * Fetch the latest version of this module.
+     *
+     * @return string
+     */
+    public function customModuleLatestVersion(): string
+    {
+        // No update URL provided.
+        if ($this->customModuleLatestVersionUrl() === '') {
+            return $this->customModuleVersion();
+        }
+
+        return Registry::cache()->file()->remember($this->name() . '-latest-version', function () {
+            try {
+                $client = new Client([
+                    'timeout' => 3,
+                ]);
+
+                $response = $client->get($this->customModuleLatestVersionUrl());
+
+                if ($response->getStatusCode() === StatusCodeInterface::STATUS_OK) {
+                    $json    = json_decode($response->getBody()->getContents(), true);
+                    $version = $json['tag_name'];
+
+                    // Does the response look like a version?
+                    if (preg_match('/^\d+\.\d+\.\d+/', $json['tag_name'])) {
+                        return $version;
+                    }
+                }
+            } catch (RequestException $ex) {
+                // Can't connect to the server?
+            }
+
+            return $this->customModuleVersion();
+        }, 86400);
+    }
+
+    public function customModuleSupportUrl(): string
+    {
+        return self::CUSTOM_SUPPORT_URL;
+    }
+
+    public function customTranslations(string $language): array
+    {
+        $languageFile = $this->resourcesFolder() . 'lang/' . $language . '/messages.mo';
+
+        return file_exists($languageFile) ? (new Translation($languageFile))->asArray() : [];
+    }
+}
